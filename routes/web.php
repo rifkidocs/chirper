41c8e1f62c6b04f6e94c7c7e08f9f8a5a0b3dac8
rifkidocs <rifkiars@gmail.com>
2026-01-06 17:49:43 +0700

    feat: add initial homepage displaying a list of chirps
    Signed-off-by: rifkidocs <rifkiars@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get(
+    '/',
+    [ChirpController::class, 'index']
+);
